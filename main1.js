d54d6b38163f17e2d4664574f83a3d2b0c210210
Catherine J <cjarocki@gmail.com>
2017-02-15 09:30:20 -0600

    Editing functions and variables and fixing other errors but ran into even more

--- main1.js
+++ main1.js
@@ -1,58 +1,71 @@
 $(document).ready(function(){
 
   // thought something like this was right for some reason, did some extra reading of resources
-  // newsflash: totally wrong!
-    // $('h2').append('<div></div>');
-    // $('h2').append('<div></div>');
-    // $('h2').append('<div></div>');
-    // $('h2').append('<div></div>');
-    //
-    // $('.box').click(function(){
-    //   $(this).toggleClass('rose');
-    //   $('.box').click(function(){
-    //     $(this).toggleClass('gold')
-    //     $('.box').click(function(){
-    //       $(this).toggleClass('green');
-    //       $('.box').click(function(){
-    //         $(this).toggleClass('blue');
-    //         });
-    //       });
-    //     });
-    //   });
-    //
-    // });
-    //so what this mess of code ended up doing was
-    // making a set of boxes (correct) that changed color when clicked
-    // but didn't correspond to anything, and didn't have the functions attached
+  // newsflash: totally wrong
+  // $('h2').append('<div></div>');
+  // $('h2').append('<div></div>');
+  // $('h2').append('<div></div>');
+  // $('h2').append('<div></div>');
+  //
+  // $('.box').click(function(){
+  //   $(this).toggleClass('rose');
+  //   $('.box').click(function(){
+  //     $(this).toggleClass('gold')
+  //     $('.box').click(function(){
+  //       $(this).toggleClass('green');
+  //       $('.box').click(function(){
+  //         $(this).toggleClass('blue');
+  //         });
+  //       });
+  //     });
+  //   });
+  //
+  // });
+  //so what this mess of code ended up doing was
+  // making a set of boxes (correct) that changed color when clicked
+  // but didn't correspond to anything, and didn't have the functions attached
 
   // make variable
   var colorBlockArray = ['rose', 'gold', 'green', 'blue'];
   // need for loop to say each time
   for (var i = 0; i < colorBlockArray.length; i++) {
-    $('#container').append('<div class='' + colorBlockArray[i] + ''></div>');
-    $('.' + colorBlockArray[i]).data('color', array[i]);
-  });
+    $('#container').append('<div class="box ' + colorBlockArray[i] + '"></div>');
+    $('.' + colorBlockArray[i]).data('color', colorBlockArray[i]);
+  };
 
   var beginningColor = chooseColor();
-  $('#color-name').append('<p>Click ' + beginningColor'</p>');
-
-  $('#container').on('click', 'div', function(){
-    if(beginningColor == $(this).data('color')){
-      initialColor = chooseColor();
-      // correct click
-      $('#colorizer > p').replaceWith('<p>You are not colorblind! Now click ' +
-      initialColor + '.</p>');
-    } else {
-      //wrong click
-      $('#colorizer > p').replaceWith('<p>Try again! Click ' +
-      initialColor + '.</p>');
-    }
-  });
+  $('#color-name').append('<p>Click ' + beginningColor + '</p>');
 
   // if ('correct color picked') {
   //   allow('play again')
   // } else {'wrong color'}
   // }
+
+  $('#container').on('click', 'div', function(){
+    for (var i = 0; i < colorBlockArray.length; i++) {
+      var beginningColor = colorBlockArray[i];
+
+
+    if(beginningColor == $(this).data('color')){
+      // correct click
+      beginningColor.data('beginningColor', i) = chooseColor();
+      beginningColor.text('You are not colorblind! Now click ' + beginningColor + '!</p>');
+      // $('#colorizer > p').replaceWith('<p>You are not colorblind! Now click ' +
+      // beginningColor + '.</p>');
+    } else {
+      //wrong click
+      beginningColor.text('<p>Try again! Click ' + beginningColor + '.</p>');
+      beginningColor.data()
+      // $('#colorizer > p').replaceWith('<p>Try again! Click ' +
+      // beginningColor + '.</p>');
+    }
+  }
+  });
+
+
+
+
+
 
   //given code - picks random number between 1 and 4
   function randomNum(){
